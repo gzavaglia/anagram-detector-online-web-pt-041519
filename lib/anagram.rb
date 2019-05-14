a3d9442ae0c9559db28d25523b883906d6c2266c
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-05-14 04:08:31 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,9 +6,9 @@ class Anagram
     @word = some_word
   end 
   
-  def match(string)
+  def match(array_of_words)
     match_array = []
-    array_of_words = string.split (" ")
+    #array_of_words = string.split (" ")
     array_of_words.each do |some_word|
       if @word.sort == some_word.sort 
         match_array << some_word
